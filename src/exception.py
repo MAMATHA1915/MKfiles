2117dd8a643be2a5868ca55323fb6798cb7632a2
MAMATHA1915 <mamathakondameedi1915@gmail.com>
2024-12-07 14:29:41 +0530

    logger and exception update

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     # Get exception information
@@ -19,4 +19,9 @@ class CustomException(Exception):
         return self.error_message
     
 
-    
+if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("diving with zero")
+        raise CustomException(e,sys)
